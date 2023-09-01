0f9fb8f3edb1451631f767421b212c9ccebde52f
valensparvoli <valensparvoli@gmail.com>
2023-09-01 20:27:53 -0300

    quienesSomos 70%

--- JS/mainScript.js
+++ JS/mainScript.js
@@ -9,3 +9,22 @@ open.addEventListener('click', ()=> {
 close.addEventListener('click', ()=>{
     nav.classList.remove('visible')
 })
+
+
+let sections = document.querySelectorAll('section')
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset=sec.offsetTop -100;
+        let height =sec.offsetHeight;
+
+        if(top >= offset && top < offset+height){
+            sec.classList.add('show-animate');
+        }
+        else{
+            sec.classList.remove('show-animate');
+        }
+
+    })
+}
